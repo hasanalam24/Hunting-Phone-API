6308f11d97c7d9185926c7d692c0336007a00740
hasanalam24 <hasanalam365@gmail.com>
2024-02-28 11:26:28 +0600

    show details

--- js/phone.js
+++ js/phone.js
@@ -15,7 +15,7 @@ const loadPhone = async (searchText, isShowAll) => {
 }
 
 const displayPhones = (phones, isShowAll) => {
-    console.log(phones)
+    // console.log(phones)
     //jeheto ekhan theke data niye onno kuthaw dekhabo na tai forEach mara hoise
 
     //step-1:
@@ -35,7 +35,7 @@ const displayPhones = (phones, isShowAll) => {
         showAllContainer.classList.add('hidden')
     }
 
-    console.log('is show all', isShowAll)
+    // console.log('is show all', isShowAll)
 
     // display only first 10 phones **if not show all
     if (!isShowAll) {
@@ -57,8 +57,8 @@ const displayPhones = (phones, isShowAll) => {
         <div class="card-body">
           <h2 class="card-title">${phone.phone_name}</h2>
            <p>If a dog chews shoes whose shoes does he  choose?</p>
-        <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+        <div class="card-actions justify-center">
+            <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
         </div>
         </div>
         `
@@ -71,7 +71,47 @@ const displayPhones = (phones, isShowAll) => {
     toggleLoadingSpinner(false)
 }
 
+//
+const handleShowDetails = async (id) => {
+    // console.log('click', id)
+    //load single phone data
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await (res.json());
+    const phone = data.data;
+    // console.log(data)
 
+    showPhoneDetails(phone)
+}
+
+const showPhoneDetails = (phone) => {
+    console.log(phone)
+
+    const phoneName = document.getElementById('show-details-phone-name');
+    phoneName.innerText = phone.name;
+
+    const showDetailContainer = document.getElementById('show-detail-container');
+
+    showDetailContainer.innerHTML = `
+    
+    <img src="${phone.image}" alt="" />
+    <p class="mt-5"><span>Brand Name : </span>${phone?.brand}</p>
+
+   
+    <p class="mt-5"><span>Storage : </span>${phone?.mainFeatures?.storage}</p>
+
+    <p class="mt-3"><span>Chip Set : </span>${phone?.mainFeatures?.chipSet}</p>
+
+    <p class="mt-3"><span>Display Size : </span>${phone?.mainFeatures?.
+            displaySize}</p>
+
+    <p class="mt-3"><span>Memory : </span>${phone?.mainFeatures?.
+            memory}</p>
+    
+    `
+
+    //show the modal
+    show_details_modal.showModal()
+}
 
 // handle search button
 const handleSearch = (isShowAll) => {
@@ -79,7 +119,7 @@ const handleSearch = (isShowAll) => {
     // console.log('click handler done')
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    console.log(searchText)
+    // console.log(searchText)
     loadPhone(searchText, isShowAll)
 }
 
